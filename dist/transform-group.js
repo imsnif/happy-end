efe4c48831f300a6fc2b215bd5c75fc2f087cd4f
Aram Drevekenin <grimsniffer@gmail.com>
2015-12-30 02:00:39 +0200

    writable, readble and tests

---
+++ dist/transform-group.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _events = require('events');
+
+var _stream = require('stream');
+
+var _stream2 = _interopRequireDefault(_stream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function finished(resolve, reject) {
+  var streamCount = 0;
+  var activeStreams = 0;
+  this.on("added", function () {
+    streamCount += 1;
+    activeStreams += 1;
+  });
+  this.on("done", function () {
+    activeStreams -= 1;
+    if (activeStreams < 0) reject("Multiple end/flush events on one or more streams.");
+    if (activeStreams === 0) {
+      resolve(streamCount);
+    }
+  });
+}
+
+function catchEnding(streamObj) {
+  var event = "";
+  if (streamObj instanceof _stream2.default.Readable) event = "end";
+  if (streamObj instanceof _stream2.default.Writable) event = "finish";
+  streamObj.on(event, this.emit.bind(this, "done"));
+}
+
+var TransformGroup = (function (_EventEmitter) {
+  _inherits(TransformGroup, _EventEmitter);
+
+  function TransformGroup() {
+    _classCallCheck(this, TransformGroup);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(TransformGroup).call(this));
+
+    _this._finished = new Promise(finished.bind(_this));
+    return _this;
+  }
+
+  _createClass(TransformGroup, [{
+    key: 'add',
+    value: function add(streams) {
+      var _this2 = this;
+
+      streams.forEach(function (streamObj) {
+        _this2.emit("added");
+        catchEnding.call(_this2, streamObj);
+      });
+      return this._finished;
+    }
+  }]);
+
+  return TransformGroup;
+})(_events.EventEmitter);
+
+exports.default = TransformGroup;
--- test/basic.js
+++ test/basic.js
@@ -1,15 +1,20 @@
 import TransformGroup from '../index.js';
 
-import fs from 'fs';
-import test from 'tape';
+import fs             from 'fs'
+import test           from 'tape'
+import DevNullStream  from 'dev-null-stream'
 
 test('read stream group', function (t) {
   t.plan(1)
   let tgroup = new TransformGroup()
-  let finished = tgroup.add([ 
-    fs.createReadStream('./lib/sample.txt'),
-    fs.createReadStream('./lib/sample2.txt')
-  ])
+  let streams = [
+    fs.createReadStream(`${__dirname}/lib/sample1.txt`),
+    fs.createReadStream(`${__dirname}/lib/sample2.txt`),
+  ]
+  streams.forEach((stream) => {
+    stream.pipe(new DevNullStream())
+  })
+  let finished = tgroup.add(streams)
   finished.then((number) => {
     t.equal(2, number)
   }).catch((reason) => {
@@ -18,13 +23,35 @@ test('read stream group', function (t) {
 })
 
 test('write stream group', function (t) {
-  t.end()
+  let tgroup = new TransformGroup()
+  let streams = [
+    fs.createWriteStream(`${__dirname}/lib/temp1`),
+    fs.createWriteStream(`${__dirname}/lib/temp3`)
+  ]
+  streams.forEach((stream) => {
+    fs.createReadStream(`${__dirname}/lib/sample1.txt`).pipe(stream)
+  })
+  let finished = tgroup.add(streams)
+  finished.then((number) => {
+    fs.unlink(`${__dirname}/lib/temp1`, (err) => {
+      if (err) t.fail(err)
+      t.equal(2, number)
+    })
+  }).catch((reason) => {
+    fs.unlink(`${__dirname}/lib/temp1`, (err) => {
+      if (err) t.fail(err)
+      t.fail(reason)
+    })
+  })
 })
 
 test('transform stream group', function (t) {
 })
 
 test('mixed group', function (t) {
+})
+
+test('not all streams end', function (t) {
 })
 
 test('multiple adds and promises', function (t) {
